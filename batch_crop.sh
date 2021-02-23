385f99a3ac6806539f779bd53393143129213008
six-two <git@15c.me>
2021-02-23 09:31:23 +0100

    Added image cropping and PDF creation scripts.

---
+++ batch_crop.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Usage: <width:height:x_offset:y_offset> <image_files...>
+
+# exit when any command fails
+set -e
+
+dimensions="$1"
+shift;
+
+path_dir=`dirname "$1"`
+new_path_dir="$path_dir/cropped"
+rm -rf $new_path_dir
+echo "Writing files to $new_path_dir"
+mkdir $new_path_dir
+
+for file in "$@"
+do
+  echo "Reading $file"
+  file_name=`basename "$file"`
+  new_file="$new_path_dir/$file_name"
+  echo "Writing $new_file"
+  convert "$file" -crop "$dimensions" "$new_file"
+  #convert "$new_file" -crop "$dimensions" "$file"
+done
---
+++ jpegs2pdf-small.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# exit when any command fails
+set -e
+
+# Config options
+TMP_FILE="/tmp/document.pdf"
+#OUTPUT_FILE="/home/$USER/document.pdf"
+OUTPUT_FILE="$1"
+shift;
+MAX_RES="1500"
+
+if [ "$#" -le 3 ]; then
+    echo "Error: Too few arguments (got only $#)"
+    echo "Usage: <output_file> <metadata_title> <image_file...>"
+    echo "Example: Certificate.pdf 'Certificate of Whatever' path/to/scanned/images/*.jpg"
+    exit 1
+fi
+
+# Document Metadata
+TITLE="$1"
+shift;
+AUTHOR="Patrick Schlueter"
+
+# Downscale images and convert them to a pdf
+convert -resize "${MAX_RES}x${MAX_RES}" -quality 50 "$@" "${TMP_FILE}"
+
+# Edit exif tags (metadata)
+exiftool -Title="$TITLE" -Author="$AUTHOR" -Producer="" "${TMP_FILE}"
+
+# Optimize the pdf
+qpdf --linearize "${TMP_FILE}" "${OUTPUT_FILE}"
