8405bc0cafd2800aa0cff2dc5d214a7bd05dfa8d
six-two <git@six-two.dev>
2021-02-24 12:48:24 +0100

    create-pdf.sh now centers files, but the image resolution is low

--- create-pdf.sh
+++ create-pdf.sh
@@ -4,6 +4,8 @@ set -e
 
 # Config options
 MAX_RES="1500"
+A4="595x842"
+RES="${MAX_RES}x${MAX_RES}"
 
 if [ "$#" -lt 2 ]; then
     echo "Usage: <image_file...> <output_file>"
@@ -12,4 +14,4 @@ if [ "$#" -lt 2 ]; then
 fi
 
 # Downscale images and convert them to a pdf
-convert -units pixelsperinch -density 72 -page a4 -resize "${MAX_RES}x${MAX_RES}" -quality 50 "$@"
+convert -units pixelsperinch -density 72  -gravity center -format pdf -resize "$A4" -extent "$A4" -page A4 -quality 50 "$@"
